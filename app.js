d93b9e404bb7bc346ef4fb72445e07b6e5559eaf
Caleb Olele <olelecaleb176@gmail.com>
2023-11-08 12:31:51 +0300

    Password Generator

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+const passWord = document.getElementById("Password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/";
+
+const allChars = upperCase + lowerCase + number + symbols;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passWord.value = password;
+}
+
+function copyPassword() {
+    passWord.select();
+    document.execCommand("copy");
+    alert("Good day! Your Password has just been copied!");
+}
